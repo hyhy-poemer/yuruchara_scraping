27e386460f0c3188183d9c656ee1add1e69db584
yuki hagiwara <kiyou345@icloud.com>
2019-07-09 14:31:53 +0900

    要らんファイル削除

--- test1.py
+++
@@ -1,8 +0,0 @@
-import lxml.html
-from urllib.request import urlopen
-
-tree = lxml.html.parse(urlopen('http://www.yurugp.jp/ranking/?year=2018'))
-html = tree.getroot()
-
-for a in html.cssselect('a'):
-    print(a.get('href'),a.text)
--- test4.py
+++
@@ -1,6 +0,0 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-
-url = 'http://www.yurugp.jp/vote/detail.php?id=00000364'
-r = requests.get(url)
